f95b94f913a307a5edfc805bba6026eefc1108ea
m15ha <mihail.lazareff@gmail.com>
2022-01-13 22:10:28 -0500

    update: react router dom v6

--- src/App.js
+++ src/App.js
@@ -1,36 +1,26 @@
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
-import { Header } from "./components/header";
-import { HomePage } from "./pages/home-page";
-import { GamePage } from "./pages/game-page";
-import { OrderPage } from "./pages/order-page";
-import { Provider } from "react-redux";
-import { store } from "./store";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Header } from './components/header';
+import { HomePage } from './pages/home-page';
+import { GamePage } from './pages/game-page';
+import { OrderPage } from './pages/order-page';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 function App() {
-  return (
-    <Provider store={ store }>
-      <Router>
-        <div className="App">
-          <Header />
-            <Switch>
-              <Route exact path="/order">
-                <OrderPage />
-              </Route>
-              <Route exact path="/app/:title">
-                <GamePage />
-              </Route>
-              <Route exact path="/">
-                <HomePage />
-              </Route>
-            </Switch>
-        </div>
-      </Router>
-    </Provider>
-  );
+    return (
+        <Provider store={store}>
+            <Router>
+                <div className='App'>
+                    <Header />
+                    <Routes>
+                        <Route path='/order' element={<OrderPage />} />
+                        <Route path='/app/:title' element={<GamePage />} />
+                        <Route path='/' element={<HomePage />} />
+                    </Routes>
+                </div>
+            </Router>
+        </Provider>
+    );
 }
 
 export default App;
